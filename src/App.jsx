deeb23175aa47660cd8a78d74d2379a57196ec50
Anthony Huarneck <anthuarneck@me.com>
2025-04-24 22:20:37 -0400

    finishes LoadingScreen component. adds component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
+import { LoadingScreen } from './components/LoadingScreen'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
 
   return (
     <>
-      
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
     </>
   )
 }
