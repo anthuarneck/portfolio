72647659ead236dbcd42d8344f5840600ba262fe
Anthony Huarneck <anthuarneck@me.com>
2025-04-29 19:14:23 -0400

    edits About component. Completes Project component structure. Adds RevealOnScroll component for fade in effect on components.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
+import { Projects } from "./components/Projects";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -23,6 +24,7 @@ function App() {
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
+        <Projects />
       </div>
     </>
   );
