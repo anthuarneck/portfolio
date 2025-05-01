f700f94ed9f4ee731b5ef4fa1d3a8f2dc0c0bd66
Anthony Huarneck <anthuarneck@me.com>
2025-04-30 20:16:31 -0400

    Contact component completed.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -25,6 +26,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </div>
     </>
   );
